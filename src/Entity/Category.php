2b5930cd3b0406b6b12f8820f7a5db79a2cb2ae3
Supay3 <elze715@gmail.com>
2021-05-02 13:12:38 +0200

    feat(Api): apprentissage de la création d'une meilleure documentation de l'Api

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Action\NotFoundAction;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
@@ -13,7 +14,25 @@ use Symfony\Component\Validator\Constraints\Length;
 /**
  * @ORM\Entity(repositoryClass=CategoryRepository::class)
  */
-#[ApiResource()]
+#[ApiResource(
+    collectionOperations: [
+        'get',
+        'post',
+    ],
+    itemOperations: [
+        'put',
+        'patch',
+        'delete',
+        'get' => [
+            'controller' => NotFoundAction::class,
+            'openapi_context' => [
+                'summary' => 'hidden',
+            ],
+            'read' => false,
+            'output' => false,
+        ],
+    ],
+)]
 class Category
 {
     /**
---
+++ src/OpenApi/OpenApiFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\OpenApi;
+
+
+use ApiPlatform\Core\OpenApi\Factory\OpenApiFactoryInterface;
+use ApiPlatform\Core\OpenApi\Model\PathItem;
+use ApiPlatform\Core\OpenApi\OpenApi;
+
+class OpenApiFactory implements OpenApiFactoryInterface
+{
+    public function __construct(private OpenApiFactoryInterface $decorated)
+    {
+    }
+
+    public function __invoke(array $context = []): OpenApi
+    {
+        $openApi = $this->decorated->__invoke($context);
+        /** @var PathItem $path */
+        foreach ($openApi->getPaths()->getPaths() as $key => $path) {
+            if ($path->getGet() && $path->getGet()->getSummary() === 'hidden') {
+                $openApi->getPaths()->addPath($key, $path->withGet(null));
+            }
+        }
+        return $openApi;
+    }
+}
