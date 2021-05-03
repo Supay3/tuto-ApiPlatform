8a31b4ad0bf7ea008bc7a3fc0527ec499a06fcc1
Supay3 <elze715@gmail.com>
2021-05-03 18:53:59 +0200

    feat(RefreshJWT): Ajout du refresh de JWT pour éviter de redemander à l'utilisateur de se reconnecter tout le temps

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
